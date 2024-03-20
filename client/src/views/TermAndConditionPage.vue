4e1c4a3b3f31673dc7f28896633157897c8e3908
Krittaphas.W <krittaphas.wi@gmail.com>
2024-03-20 15:04:07 +0700

    :lipstick: update(view): no toolbar and navpane for pdf preview

--- client/src/views/TermAndConditionPage.vue
+++ client/src/views/TermAndConditionPage.vue
@@ -6,7 +6,7 @@
 					<div class="card">
 						<strong>แนวนโยบายและแนวปฏิบัติการให้บริการใบรับรองอิเล็กทรอนิกส์</strong>
 						<iframe width="100%" height="650px"
-						:src="pdfFile+'#page=1&zoom=100'">
+						:src="pdfFile+'#page=1&zoom=100&toolbar=0&navpanes=0&scrollbar=0'">
 						</iframe>
 					</div>
 				</div>
